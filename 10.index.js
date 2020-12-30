bd500c5bdb627d13a3102c1df0b8f21c3cd71a17
hanyugeon <hanyugeon@naver.com>
2020-12-30 19:01:26 +0900

    20.12.30 study_02

--- 10.index.js
+++ 10.index.js
@@ -1,3 +1,7 @@
 // HTML이 JS와 어떻게 함께 작동하는지 알아보는 시간.
 const title = document.getElementById("title");
 title.innerHTML = "Hi! From JS";
+
+// HTML을 DOM 객체로 바꿀 수 있다.
+// DOM? wtf is this...
+// DOM : document object model
