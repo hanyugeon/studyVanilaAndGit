e1d424809d0f452a72c4d442525dd7586a9d2404
hanyugeon <hanyugeon@naver.com>
2021-01-16 19:56:00 +0900

    21.01.16 study_01

--- jsClock/greeting.js
+++ jsClock/greeting.js
@@ -3,37 +3,37 @@ const input = form.querySelector("input"); // clock.html 12line 참고
 const greeting = document.querySelector(".js-greetings"); // clock.html 14line 참고
 
 const USER_LS = "currentUser"; // localStorage로 저장할 시에 key값으로 됨
-const SHOWING_CN = "showing";
+const SHOWING_CN = "showing"; // ??? wtf is this?
 
 function saveName(text) {
-    localStorage.setItem(USER_LS, text);
+    localStorage.setItem(USER_LS, text); // localStorage에 저장.
 }
 
 function handleSubmit(event) {
-    event.preventDefault();
+    event.preventDefault(); // preventDefault() : 원래 설정 및 실행을 억제
     const currentValue = input.value;
     console.log(currentValue);
     paintGreeting(currentValue);
     saveName(currentValue);
 }
 
-function askForName() {
+function askForName() { // 이후에 공부가 필요한 부분(EventListener : submit)
     form.classList.add(SHOWING_CN);
     form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
-    form.classList.remove(SHOWING_CN)
-    greeting.classList.add(SHOWING_CN); // 3-3 부터
-    greeting.innerText = `Hello ${text}`
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
 }
 
 function loadName() {
-    const currentUser = localStorage.getItem(USER_LS);
+    const currentUser = localStorage.getItem(USER_LS); // USER_LS의 value값
     if(currentUser === null) {
-        askForName();
+        askForName(); // value값이 null일때 새로 입력을 받는 함수 호출
     } else {
-        paintGreeting(currentUser);
+        paintGreeting(currentUser); // value값을 불러오는 함수 호출
     }
 }
 
@@ -42,5 +42,3 @@ function init() {
 }
 
 init();
-
-// local storage?
