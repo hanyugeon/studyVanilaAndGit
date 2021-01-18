13930d63d37b42efcb18426417176547a293d344
Kim Jae Hyeon <73218463+hanyugeon@users.noreply.github.com>
2021-01-18 19:51:48 +0900

    21.01.18 study_03

--- jsToDo_1.02ver/todo.js
+++ jsToDo_1.02ver/todo.js
@@ -5,18 +5,34 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+    // 사실 localStorage에는 string만 저장할 수 있다.
+    // JSON.stringify 활용하기.
+}
+
 function paintToDo(text) {
     const li = document.createElement("li");
     const deleteButton = document.createElement("button");
-    deleteButton.innerText = "❎";
     const checkButton = document.createElement("button");
-    checkButton.innerText = "✅"
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    deleteButton.innerText = "❎";
+    checkButton.innerText = "✅"
     span.innerText = text;
     li.appendChild(span);
     li.appendChild(deleteButton);
     li.appendChild(checkButton);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos(); // 함수 위치가 왜 여기인지 생각해보기.
 }
 
 function handleSubmit(event) {
@@ -27,8 +43,18 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null) {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null) {
+        /*
+        console.log(loadedToDos);
+        // 근데 새로고침 이후 다시 불러온게 string임.
+        */
+        const parsedToDos = JSON.parse(loadedToDos);
+        console.log(parsedToDos);
+        // JSON.parse 활용하기.
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
     } 
 }
 
