9ad72c2e634c2e0cf3a32d004ef0027757e2fff7
hanyugeon <hanyugeon@naver.com>
2021-09-15 15:58:38 +0900

    recap learn about const, let, var

--- 01.hello_world.js
+++ 01.hello_world.js
@@ -1,11 +1,14 @@
-alert('im vanila js, im beatiful, im awesome as fxck');
+alert('welcome to recap world.');
 
- let a = 221;
- let b = a - 5;
- a = 4; // just updated., 위에서 아래로.
- console.log(b, a);
- /**
-  * 변수 생성
-  * 초기화
-  * 사용하기
-  */
+let a = 74;
+let b = a - 5;
+const c = 1; // just updated., 위에서 아래로.
+
+console.log(b, a);
+/**
+ * const: 블록단위(불변)
+ * let: 블록단위(가변)
+ * var: 함수단위(가변)
+ * 자세히 알아보기
+ * https://blog.naver.com/hanyugeon/222500632353
+ */
