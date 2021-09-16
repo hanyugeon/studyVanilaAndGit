746142c8dcbe8b2458c181c93d02eaaeb69e6131
hanyugeon <hanyugeon@naver.com>
2021-09-16 19:17:14 +0900

    recap_make code clean

--- Recap/src/clock/clock.js
+++ Recap/src/clock/clock.js
@@ -1,25 +1,26 @@
-const clockContainer = document.querySelector(".js-clock");
-const clockTitle = clockContainer.querySelector(".js-clockTitle")
+const clockBlock = document.querySelector(".js-clockBlock");
+const clock = clockBlock.querySelector(".js-clock");
 
 function getTime() {
+    // 변수 선언부
     const date = new Date();
     const minutes = date.getMinutes();
     const hours = date.getHours();
 
+    // 10 미만단위 앞에 0 붙이기
     if(hours < 10) {
         if(minutes < 10) {
-            clockTitle.innerText = `0${hours}:0${minutes}`;
+            clock.innerText = `0${hours}:0${minutes}`;
         } else {
-            clockTitle.innerText = `0${hours}:${minutes}`;
+            clock.innerText = `0${hours}:${minutes}`;
         }    
     } else {
         if(minutes < 10) {
-            clockTitle.innerText = `${hours}:0${minutes}`;
+            clock.innerText = `${hours}:0${minutes}`;
         } else {
-            clockTitle.innerText = `${hours}:${minutes}`;
+            clock.innerText = `${hours}:${minutes}`;
         }
     }
-
 }
 
 function init() {
